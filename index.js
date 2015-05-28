829d4f4198ef0c95fc00cf4a14af1c8853889b7a
Mohammad Anwari <mdamt@mdamt.net>
2015-05-29 06:03:50 +0700

    Add hapi-swagger

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ var server = new Hapi.Server();
 server.connection({ port: 3000, labels: ['api'] });
 sources.populate(server);
 
+server.register({
+    register: require('hapi-swagger'),
+    options: {
+        apiVersion: "1.0"
+    }
+}, function (err) {
+    if (err) {
+        server.log(['error'], 'hapi-swagger load error: ' + err)
+    }else{
+        server.log(['start'], 'hapi-swagger interface loaded')
+    }
+});
+
 server.start(function () {
   console.log('Server running at:', server.info.uri);
 });
