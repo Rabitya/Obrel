ce8bab78e74d9c927d2eaac0a864f910c6e591d7
Fitra Aditya <fitra@g.pl>
2015-07-03 04:04:42 +0700

    update: prettify code, change whitespace and indentation

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var Hapi = require('hapi');
-var sources = require("./sources/");
-
+var sources = require('./sources/');
 var server = new Hapi.Server();
+
 server.connection({ port: 3000, labels: ['api'] });
 sources.populate(server);
 
-server.start(function () {
+server.start(function() {
   console.log('Server running at:', server.info.uri);
 });
