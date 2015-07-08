a398c751166951b2b5d71be7f7c9cb8b9a1bd4ce
Fitra Aditya <fitra@g.pl>
2015-07-08 19:42:47 +0700

    update: change modules dir. read in node_modules instead of sources

--- sources/index.js
+++ sources/index.js
@@ -1,8 +1,49 @@
-var fs = require("fs");
+var fs = require('fs');
 
 var populate = function(server) {
   var sources = [];
-  var dirs = fs.readdirSync(__dirname);
+  var modules = [];
+  var modules_dir = null;
+
+  try {
+    modules = fs.readdirSync(__dirname + '/node_modules');
+    modules_dir = __dirname + '/node_modules';
+  } catch (ex) {
+    modules = fs.readdirSync(__dirname + '/../node_modules');
+    modules_dir = __dirname + '/node_modules';
+  }
+
+  modules.forEach(function(module) {
+    if (module.indexOf('obrel-') == 0) {
+      console.log('Loading: ', module);
+      
+      var settings = {};
+      
+      if (fs.existsSync(modules_dir + '.settings.json')) {
+        settings = require(modules_dir + '.settings.json');
+      }
+      
+      if (fs.existsSync(modules_dir + '/index.js')) {
+        var routeSettings = {};
+
+        if (settings.prefix) {
+          routeSettings.prefix = settings.prefix;
+        }
+
+        server.register({
+          register: require(dir),
+          options: {}
+        }, {
+          select: ['api'],
+          routes: routeSettings
+        }, function(err) {
+          if (err) {
+            console.log('Error registering sources ' + dir, err);
+          }
+        });
+      }
+    }
+  });
 
   for (var i in dirs) {
     var dir = __dirname + '/' + dirs[i];
